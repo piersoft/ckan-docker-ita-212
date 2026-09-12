17755d3a194bd0f39bd9428f25ef6fde9e2142c9
piersoft <piersoft@users.noreply.github.com>
2026-09-12 14:52:02 +0000

    oai-pmh: patch pyoai (pkg_resources) a build time

---
+++ ckan/patches/patch_pyoai.py
@@ -0,0 +1,16 @@
+"""pyoai (2013) importa pkg_resources solo per leggere la propria versione nel
+tag <toolkit> di Identify; i setuptools recenti non lo forniscono piu'.
+Eseguito a build time dal Dockerfile dopo l'installazione di ckanext-oai-pmh-server."""
+import os
+import re
+
+import oaipmh
+
+p = os.path.join(os.path.dirname(oaipmh.__file__), 'common.py')
+s = open(p).read()
+s = s.replace("import pkg_resources\n", "")
+s = re.sub(r"req = pkg_resources\.Requirement\.parse\('pyoai'\)\n\s*egg = pkg_resources\.working_set\.find\(req\)",
+           "egg = None  # ckan-docker-ita: pkg_resources non disponibile", s)
+open(p, 'w').write(s)
+assert 'pkg_resources' not in s, 'patch pyoai fallita'
+print('pyoai patched OK:', p)
